011b26283e3250e1a2fd26eea834c320acfbcedb
Sofiane Sahraoui <sofiane.sahraoui17@gmail.com>
2025-02-25 23:18:11 +0100

    about me

---
+++ src/components/Main6.jsx
@@ -0,0 +1,15 @@
+import React from "react"; 
+import img from "../assets/img/aboutMe.png"
+
+function Main6 () {
+    return(
+        <div>
+            <img src={img} alt="image du dev" />
+        <h2>about me</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos iste accusamus quod, reprehenderit corrupti sequi eaque consequuntur enim harum vitae.</p>
+
+        </div>
+    )
+}
+
+export default Main6 
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -5,6 +5,7 @@ import Main2 from '../components/Main2';
 import Main3 from '../components/Main3';
 import Main4 from '../components/Main.4';
 import Main5 from '../components/Main5';
+import Main6 from '../components/Main6';
 
 function Home ()
 {
@@ -16,6 +17,7 @@ function Home ()
             <Main3/>
             <Main4/>
             <Main5/>
+            <Main6/>
         </div>
     )
 }
