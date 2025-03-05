0eb215b7379f5bacc1f6853e142fc04b6e71dd60
Sofiane Sahraoui <sofiane.sahraoui17@gmail.com>
2025-03-05 17:36:37 +0100

    fin du main7

--- src/components/Main7.jsx
+++ src/components/Main7.jsx
@@ -1,5 +1,7 @@
 import React from "react"; 
 import ComposantMain7 from "./ComposentMain7";
+import Button from "./Button";
+import img from "../assets/img/ImagePetiteTablette2.png"
 
 
 
@@ -16,6 +18,24 @@ function Main7() {
         <ComposantMain7 titre="Do I need to pay to use Notion ?"/>
         <ComposantMain7 titre="What whill I get when I buy the template ?"/>
         <ComposantMain7 titre="How can I ask more questions ?"/>
+    </div>
+
+    <div className="bg-[#F6F5F4] p-2 rounded-xl ml-4 mr-4 w-[700px] mt-18">
+      <div className="grid grid-cols-2">
+        <div className=" p-4 flex flex-col justify-center items-center">
+            <h3 className="font-extrabold text-3xl text-center mt-2 mb-2">The ultimate 
+                <span className="inline-block text-center"> meal planner
+                    
+                    </span>
+                    </h3>
+            <p className="text-center">Supercharge your health and take control of your diet now!</p>
+            <Button/>
+        </div>
+        <div className="p-4">
+            <img src={img} alt="image tablette" />
+         
+        </div>
+      </div>
     </div>
     </div>
     )
